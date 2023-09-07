526dbc7e5ad7b3b64efe266cd42e05d10226804a
bbeck9123 <byron.ptgo@gmail.com>
2023-09-07 15:56:13 -0700

    anki

---
+++ server.js
@@ -0,0 +1,14 @@
+const square = num =  Number(num.toString().split('').map(e=>e*e).join(''))
+
+function square(num){
+    let numb = num.toString()
+    let sqr = []
+
+    for(let i=0;i< numb.length;i++){
+        sqr.push(numb[i]*numb[i])
+    }
+
+    return Number(sqr.join(''))
+
+}
+
